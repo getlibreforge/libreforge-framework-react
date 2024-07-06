fb2b328919c2baf119ba6333b8d01e6c7889eb52
Admin <getlibreforge@healthentech.com>
2024-07-06 17:55:13 -0400

    CHANGE: props override example

--- src/components/Button/ButtonComponent.tsx
+++ src/components/Button/ButtonComponent.tsx
@@ -2,13 +2,16 @@ import { Button } from '@chakra-ui/react';
 import { iconsList } from '../../iconsList';
 import { forwardRef } from 'react';
 import { IComponents, IPages } from '@libreforge/libreforge-framework-shared';
-import { cleanupCustomComponentProps, useActionHandlers } from '@libreforge/libreforge-framework';
+import { cleanupCustomComponentProps, useActionHandlers, usePropsOverrideByComponentRef } from '@libreforge/libreforge-framework';
 
-const ButtonComponent = forwardRef((props: { componentId: string, pages: IPages, 
+const ButtonComponent = forwardRef((props: { componentId: string, pages: IPages, designMode: boolean, 
     pageComponents: IComponents, componentPage: string, collectionRefIdx: number | undefined }, ref) => {
 
+  let targetProps: any = props;
+
   const actionGroup = props.pageComponents[props.componentId].actionGroup;
-  let targetProps = useActionHandlers(props, actionGroup);
+  targetProps = useActionHandlers(targetProps, actionGroup);  
+  targetProps = usePropsOverrideByComponentRef(props.componentId, targetProps, props.designMode);
 
   if (targetProps.leftIcon) {
     if (Object.keys(iconsList).includes(targetProps.leftIcon)) {
